fbc81afff006f5bab03874529b8a26dcdf109dd8
Hritish Parasar <hritishparasar07@gmail.com>
2025-10-13 10:49:50 +0530

    Added Models

---
+++ StockOne.API/Model/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace StockOne.API.Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Author { get; set; } = string.Empty;
+        public int StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
